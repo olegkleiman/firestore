76fd22c6286099dd19606e9134de0a93930bd5f2
Oleg Kleiman <oleg_kleyman@hotmail.com>
2017-11-05 20:37:17 +0200

    Add files via upload

---
+++ js/Header.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import { render } from 'react-dom';
+import { injectIntl } from 'react-intl';
+
+import styles from '../css/header.css'
+
+class Header extends React.Component {
+
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (<div>
+              <header id="app_header" className={styles.headerWaterfall + ' mdl-layout__header mdl-layout__header--scroll'}
+                      aria-haspopup="true" aria-label={this.props.intl.messages.main_menu}>
+                  <div className='mdl-layout__header-row'>
+                      {/* Title */}
+                      <span className='mdl-layout-title' role='banner'>
+                        <a href="#/" className={styles.odsCaption}>TLV OpenData</a>
+                      </span>
+
+                      <div className='mdl-layout-spaces'></div>
+                      <div className='mdl-layout-spaces'></div>
+                      <div className='mdl-layout-spaces'></div>
+
+                      <a className={styles.headerButton} href='/'>
+                        {this.props.intl.messages.language}
+                      </a>
+
+                  </div>
+              </header>
+            </div>);
+  }
+
+};
+
+export default injectIntl(Header);
