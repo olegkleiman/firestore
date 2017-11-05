274950559130b7f6bf8ee10b8aba91f5412ff223
Oleg Kleiman <oleg_kleyman@hotmail.com>
2017-11-06 00:11:45 +0200

    Update template.js

--- template.js
+++ template.js
@@ -1,13 +1,16 @@
 export default ({content, state}) => {
   return `
       <!DOCTYPE html>
-      <html lang="he">
+      <html dir="rtl" lang="he">
       <head>
         <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0" />
         <title>תל אביב-יפו TLV OpenData</title>
         <link rel='icon' type='image/x-icon' href='../favicon.ico' />
         <link rel='shortcut icon' href='../favicon.ico' />
-        <link href='styles.css' rel='stylesheet'></style>
+        <link href='styles.css' rel='stylesheet' />
+
+        <link href='material.rtl.css' rel='stylesheet' />
        </head>
       <body>
         <div id="app">${content}</div>
