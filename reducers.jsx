df49f85a05e58afc2e7f1ee58b1172474851794d
Oleg Kleiman <oleg_kleyman@hotmail.com>
2017-11-06 00:04:27 +0200

    Add files via upload

---
+++ reducers.jsx
@@ -0,0 +1,43 @@
+import _ from 'lodash'
+
+const update = (state, mutations) => {
+  _.assign({}, state, mutations);
+};
+
+const INITIAL_STATE = {
+  categories: [],
+  markers: [],
+  selectedCategory: 0
+};
+
+const reducers = (state = INITIAL_STATE, action) => {
+
+  //console.log('Reducer: ' + action.type + ' Data: ' + action.data);
+
+  switch( action.type ) {
+    case 'CATEGORIES_INIT':
+      return {
+          ...state,
+          categories: state.categories.concat(action.data)
+      };
+
+    case 'MARKERS_INIT':
+      return {
+          ...state,
+          markers: state.markers.concat(action.data)
+      };
+
+    case 'CATEGORY_CHANGED':
+      //state = update(state, {selectedCategory: action.data} );
+      state = _.assign({}, state, {selectedCategory: action.data});
+      break;
+
+    default:
+      break;
+  };
+
+  return state;
+
+};
+
+export default reducers;
