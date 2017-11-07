efd64ef12a270e9c6266351866218e607659edb4
Oleg Kleiman <oleg_kleyman@hotmail.com>
2017-11-07 23:44:10 +0200

    Update template.js

--- template.js
+++ template.js
@@ -10,13 +10,19 @@ export default ({content, state, language}) => {
         <link rel='shortcut icon' href='../favicon.ico' />
         <link href='styles.css' rel='stylesheet' />
 
-        <link href='material.rtl.css' rel='stylesheet' />
+        <link rel="stylesheet"
+            href='https://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css'>
+        <link rel="stylesheet"
+            href="https://fonts.googleapis.com/icon?family=Material+Icons">
+        <link rel="stylesheet"
+          href="https://code.getmdl.io/1.3.0/material.indigo-pink.min.css">
        </head>
       <body>
         <div id="app">${content}</div>
         <script>
           window.__PRELOADED_STATE__ = ${JSON.stringify(state)};
         </script>
+        <script defer src="https://code.getmdl.io/1.3.0/material.min.js"></script>
         <script id='bundle' src="http://localhost:8080/bundle.js" lang='${language}'></script>
       </body>
       </html>
