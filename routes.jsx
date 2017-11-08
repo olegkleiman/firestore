07a704e44c661e8611da52f08e4048827a817d5e
Oleg Kleiman <oleg_kleyman@hotmail.com>
2017-11-08 20:03:51 +0200

    Update routes.jsx

--- routes.jsx
+++ routes.jsx
@@ -2,7 +2,12 @@ import React from 'react';
 import { Route, IndexRoute } from 'react-router';
 
 import App from './js/App.jsx';
+import DefaultLayout from './js/DefaultLayout.jsx';
+import Dataset from './js/Dataset.jsx';
 
 module.exports = (
-  <Route path='/' component={App} />
+  <Route path='/' component={App}>
+      <IndexRoute component={DefaultLayout} />
+      <Route path='/ds/:id' component={Dataset} />
+  </Route>
 )
