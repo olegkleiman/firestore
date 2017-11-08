f171ea1e7d92446a52cf93a82d3e1951576cfb0a
Oleg Kleiman <oleg_kleyman@hotmail.com>
2017-11-08 20:03:24 +0200

    Update Promotion.jsx

--- js/Promotion.jsx
+++ js/Promotion.jsx
@@ -3,9 +3,20 @@ import styles from '../css/promotion.css'
 
 class Promotion extends React.Component {
 
+  constructor(props) {
+
+    super(props);
+
+    this.navigate = this.navigate.bind(this);
+  }
+
+  navigate() {
+    this.props.router.push('/ds/222');
+  }
+
   render() {
 
-    return (<div className={styles.item + ' mdl-grid mdl-cell mdl-cell--12-col'}
+    return (<div onClick={this.navigate} className={styles.item + ' mdl-grid mdl-cell mdl-cell--12-col'}
                   role='button' tabIndex='0'>
                   <div className={styles.itemText + ' mdl-cell mdl-cell--10-col-desktop mdl-cell--3-col-phone mdl-cell--stretch mdl-cell--middle'}>
                     {this.props.name}
